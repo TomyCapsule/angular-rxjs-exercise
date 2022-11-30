2d6afcade91f9c4857abb6124ce58ff64d3a6cbc
TomyCapsule <102286385+TomyCapsule@users.noreply.github.com>
2022-11-30 12:24:55 +0100

    TODO: Exercice 7

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Subject, combineLatest, merge, race, interval } from 'rxjs';
+import { Subject, combineLatest, merge, race, interval, concat, zip } from 'rxjs';
 import {
   filter,
   distinct,
@@ -13,6 +13,7 @@ import {
   delay,
   debounceTime,
   mergeMap,
+  switchMap,
 } from 'rxjs/operators';
 
 import { Car } from './types/car.type';
@@ -30,6 +31,13 @@ const trucks$ = new Subject<Truck>();
 // step 2: only get the 'color' of the car
 // step 3: only emit a new value when the value is different from the previous one
 
+cars$.pipe(
+  filter(car => ['red','black'].includes(car.color)),
+  map(car => car.color),
+  distinctUntilChanged(),
+  tap(console.log),
+)
+
 // step 1: --c1--c2--c3--c4--c5--c6--c7--c8--c9--c10--c11--c12--c13
 // step 2: --c1--c2------c4
 
@@ -37,38 +45,81 @@ const trucks$ = new Subject<Truck>();
 // step 1: skip the first 3 cars from the stream
 // step 2: take only the first 5 cars from the stream, ignore all the others
 
+cars$.pipe(
+  skip(3),
+  take(5),
+  tap(console.log)
+)
+
 // #3 ---
 // step 1: only get the cars with the 'color' blue
 // step 2: console log the cars inside the stream
 // step 3: delay the emit of the values by 500ms
 
+cars$.pipe(
+  filter(car => car.color === 'blue'),
+  tap(console.log),
+  tap(console.time),
+  delay(500),
+  tap(console.timeEnd)
+)
+
 // #4 ---
 // step 1: only get the 'make' of the car
 // step 2: only emit a new value when there hasn't been any activity on the stream for at least 500ms
 
+cars$.pipe(
+  map(car => car.make),
+  debounceTime(500),
+  tap(console.log)
+)
+
 // #5 ---
 // step 1: combine all cars with all trucks (don't use the 'merge' operator)
 
+combineLatest(cars$, trucks$).pipe(
+  tap(console.log)
+)
+
 // #6 ---
 // step 1: merge all cars with all trucks (don't use the 'combineLatest' operator)
+
+merge(cars$, trucks$).pipe(
+  tap(console.log)
+)
 
 // #7 ---
 // step 1: merge all cars with all trucks (don't use the 'combineLatest' operator)
 // step 2: make sure that the trucks output before the cars
 
+
+
 // #8 ---
 // step 1: only get the cars by make Ford and Volvo
 // step 2: merge the trucks into the cars stream and make sure only the trucks of the same brand as the cars will be in the stream output (HINT: take a look at mergeMap and switchMap, only the trucks should be returned)
 
+cars$.pipe(
+  filter(car => ['Ford', 'Volvo'].includes(car.make)),
+  map(car => car.make),
+  mergeMap(make => trucks$.pipe(
+    filter(truck => truck.make === make)
+  ))
+)
+
 // #9 ---
 // step 1: only emit the results of whoever emits first, if cars$ emits first then the trucks$ should be ignored completely
+
+race(cars$, trucks$)
 
 // #10 ---
 // step 1: the first value of cars$ should be combined with the first value of trucks$
 // cars$ = [1,2,3,4,5]; trucks$ = ['a', 'b', 'c']; result = [ [1,'a'], [2,'b'], [3,'c'] ]
 
+zip(cars$, trucks$)
+
 // #11 ---
 // step 1: log something every 2000ms (the value can be a static value)
+interval(2000)
 
 // DO NOT REMOVE
 cars$.next({ id: 'c1', make: 'BMW', model: 'M5', color: 'red' });
